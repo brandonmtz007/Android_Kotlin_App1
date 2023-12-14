d306746d6653aa312978fa2737a6eee371eb4ad3
brandonmtz007 <brandonmtz179@gmail.com>
2023-12-14 03:18:08 -0600

    initial commit

---
+++ app/src/main/java/com/example/projecto002intento2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.projecto002intento2
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val et1=findViewById<EditText>(R.id.et1)
+        val et2=findViewById<EditText>(R.id.et2)
+        val tv1=findViewById<TextView>(R.id.tv1)
+        val button=findViewById<Button>(R.id.boton)
+        button.setOnClickListener {
+            val et1=findViewById<EditText>(R.id.et1)
+            val nro1 = et1.text.toString().toInt()
+            val nro2 = et2.text.toString().toInt()
+            val suma = nro1 + nro2
+            tv1.text = "Resultado: ${suma.toString()}"
+        }
+
+    }
+}
